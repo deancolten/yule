9db13d9d15d202f0e742edad0d79f2d80d2e4f77
Colten Dean <coltenrdean@gmail.com>
2021-01-22 16:17:30 -0600

    setup ver update

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 setuptools.setup(
     name="yule",
-    version='0.0.4',
+    version='0.0.5',
     description="A logging package for python",
     long_description=Path("README.md").read_text(),
     long_description_content_type='text/markdown',
